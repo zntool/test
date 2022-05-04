a4c0ff76968047b7268822fc2e8f3a14c4d1eeb7
ZnDeveloper <zndev@ya.ru>
2022-05-04 22:58:02 +0600

    upd

--- src/Asserts/BaseAssert.php
+++ src/Asserts/BaseAssert.php
@@ -2,15 +2,17 @@
 
 namespace ZnTool\Test\Asserts;
 
-use ZnCore\Base\Enums\Http\HttpHeaderEnum;
-use ZnCore\Base\Legacy\Yii\Helpers\ArrayHelper;
-use ZnCore\Domain\Entities\DataProviderEntity;
-use ZnLib\Rest\Helpers\RestResponseHelper;
-use ZnTool\Test\Helpers\RestHelper;
 use PHPUnit\Framework\TestCase;
-use Psr\Http\Message\ResponseInterface;
+use ZnCore\Base\Legacy\Yii\Helpers\ArrayHelper;
+use ZnTool\Test\Helpers\RestHelper;
 
 abstract class BaseAssert extends TestCase
 {
 
+    public function assertItemsByAttribute(array $values, string $attributeName, array $collection) {
+        $actualIds = ArrayHelper::getColumn($collection, $attributeName);
+        sort($values);
+        sort($actualIds);
+        $this->assertEquals($values, $actualIds);
+    }
 }
---
+++ src/Asserts/DataTestCase.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace ZnTool\Test\Asserts;
+
+use Illuminate\Support\Collection;
+use ZnCore\Domain\Helpers\EntityHelper;
+
+class DataTestCase extends BaseAssert
+{
+
+    public static function loadFromJsonFile(string $fileName) {
+        return json_decode(file_get_contents($fileName), JSON_OBJECT_AS_ARRAY);
+    }
+
+    public static function saveToJsonFile(string $fileName, $data) {
+        if($data instanceof Collection) {
+            $data = EntityHelper::collectionToArray($data);
+        }
+        file_put_contents(json_encode($data, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+    }
+}
