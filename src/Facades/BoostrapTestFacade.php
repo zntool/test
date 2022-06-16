479e95c6bf69d541d14466e5c545d0051e125225
Vitaliy <theyamshikov@yandex.ru>
2022-06-16 10:10:48 +0600

    upd

---
+++ src/Facades/BoostrapTestFacade.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace ZnTool\Test\Facades;
+
+use Illuminate\Container\Container;
+use ZnCore\Base\Libs\Container\Helpers\ContainerHelper;
+use ZnCore\Base\Libs\Container\Interfaces\ContainerConfiguratorInterface;
+use ZnSandbox\Sandbox\App\Interfaces\AppInterface;
+use ZnSandbox\Sandbox\App\Libs\ZnCore;
+use ZnTool\Test\Libs\TestApp;
+
+class BoostrapTestFacade
+{
+
+    public static function init(array $bundles): AppInterface
+    {
+        $container = ContainerHelper::getContainer() ?: new Container();
+        $znCore = new ZnCore($container);
+        $znCore->init();
+
+        /** @var ContainerConfiguratorInterface $containerConfigurator */
+        $containerConfigurator = $container->get(ContainerConfiguratorInterface::class);
+        $containerConfigurator->singleton(AppInterface::class, TestApp::class);
+
+        /** @var AppInterface $appFactory */
+        $appFactory = $container->get(AppInterface::class);
+        return $appFactory;
+
+    }
+}
