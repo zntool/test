ad9a1c14790c3a1b8114c121d3d7f8ed09cb8c0c
Vitaliy <theyamshikov@yandex.ru>
2022-06-27 17:44:24 +0600

    upd

--- src/Traits/AssertTrait.php
+++ src/Traits/AssertTrait.php
@@ -2,6 +2,9 @@
 
 namespace ZnTool\Test\Traits;
 
+use App\Gosp\Message\Domain\Entities\ResponseEntity;
+use Illuminate\Support\Enumerable;
+use ZnCore\Base\Validation\Helpers\ErrorCollectionHelper;
 use ZnCore\Domain\Entity\Helpers\CollectionHelper;
 use ZnCore\Base\FileSystem\Helpers\FileStorageHelper;
 use ZnLib\Components\Store\Helpers\StoreHelper;
@@ -19,10 +22,28 @@ trait AssertTrait
         $this->assertArraySubset($expected, $arr);
     }
 
-    protected function assertEntity($expected, object $entity)
+    public function assertCollection(array $expectedResult, Enumerable $collection)
+    {
+        $this->assertArraySubset($expectedResult, CollectionHelper::toArray($collection));
+        $this->assertCount(count($expectedResult), $collection->getIterator());
+        return $this;
+    }
+
+    /*public function assertUnprocessable(UnprocessibleEntityException $exception)
+    {
+        $errorCollection = $exception->getErrorCollection();
+        $errors = ErrorCollectionHelper::collectionToArray($errorCollection);
+        $this->assertArraySubset($err, $errors);
+        return $this;
+    }*/
+
+    protected function assertEntity($expected, object $entity, string $instanceOf = null)
     {
         $arr = EntityHelper::toArray($entity);
         $this->assertArraySubset($expected, $arr);
+        if($instanceOf) {
+            $this->assertInstanceOf($instanceOf, $entity);
+        }
     }
 
 
