9df443737300b206ef5d9b4b9379f84a3a1c78f2
ZnDeveloper <zndev@ya.ru>
2022-07-05 17:55:27 +0600

    upd

--- src/Asserts/DataTestCase.php
+++ src/Asserts/DataTestCase.php
@@ -2,19 +2,21 @@
 
 namespace ZnTool\Test\Asserts;
 
+use ZnCore\Domain\Collection\Interfaces\Enumerable;
 use ZnCore\Domain\Collection\Libs\Collection;
 use ZnCore\Domain\Entity\Helpers\CollectionHelper;
-use ZnCore\Domain\Entity\Helpers\EntityHelper;
 
 class DataTestCase extends BaseAssert
 {
 
-    public static function loadFromJsonFile(string $fileName) {
+    public static function loadFromJsonFile(string $fileName)
+    {
         return json_decode(file_get_contents($fileName), JSON_OBJECT_AS_ARRAY);
     }
 
-    public static function saveToJsonFile(string $fileName, $data) {
-        if($data instanceof Collection) {
+    public static function saveToJsonFile(string $fileName, $data)
+    {
+        if ($data instanceof Enumerable) {
             $data = CollectionHelper::toArray($data);
         }
         file_put_contents(json_encode($data, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
